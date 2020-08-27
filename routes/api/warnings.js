b1d382a7f0779cf88689da38f9e8c6fad7a7da38
Arnav Jain <arnav@dentaledglobal.com>
2020-08-28 00:33:16 +1000

    Weather warnings API: adding tags for each warning

--- routes/api/warnings.js
+++ routes/api/warnings.js
@@ -10,8 +10,34 @@ const parser = new Parser();
   router.get("/", (req, res) => {
     // Parsing the warnings data for Victoria
     parser.parseURL('http://www.bom.gov.au/fwo/IDZ00059.warnings_vic.xml', function(err, parsed) {
+
+        var warnings_data = [];
+
+        parsed.items.forEach(function(item, index){
+          var tag = "";
+          if (item.title.toLowerCase().includes("flood")) {
+            tag = "Flood";
+          } else if (item.title.toLowerCase().includes("frost")) {
+            tag = "Frost";
+          } else if (item.title.toLowerCase().includes("sheep")) {
+            tag = "Sheep";
+          } else if (item.title.toLowerCase().includes("wind")) {
+            tag = "Wind";
+          } else if (item.title.toLowerCase().includes("severe weather")) {
+            tag = "Severe Weather";
+          }
+
+          warnings_data.push({
+            "title": item.title,
+            "link": item.link,
+            "pubDate": item.pubDate,
+            "tag": tag,
+            "isoDate": item.isoDate
+          })
+        });
+
         // Returning the json array of all warnings issued.
-        res.json(parsed.items);
+        res.json(warnings_data);
     });
   })
 
